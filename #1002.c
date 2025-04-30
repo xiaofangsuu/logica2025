9f80111c0f8f8e1a9c464b40005843360948ce43
xiaofangsuu <su.xiaofang@unifesp.br>
2025-04-30 11:14:47 -0300

    #1002.c

---
+++ #1002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    double raio,area,pi;
+    
+    pi = 3.14159;
+    
+    scanf("%lf",&raio);
+    area = pi*raio*raio;
+    
+    printf("A=%.4f\n",area);
+
+    return 0;
+}
